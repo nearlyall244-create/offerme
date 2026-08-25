5bfc56d20250dba3edc7ec0dadfb91f2c096e874
nearlyall244-create <nearlyall244@gmail.com>
2026-08-25 11:45:58 +0530

    updated

--- fire/new/backend/src/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { authenticate } from '../middleware/authenticate.js';
-import { getProfile } from '../controllers/userController.js';
-
-const router = Router();
-
-router.get('/profile', authenticate, getProfile);
-
-export default router;
